8d854401a15051e247d19a1314e7df51a940d19a
BearD01001 <dino@beard.ink>
2018-03-04 16:04:21 +0800

    Add tpl files.

---
+++ src/Container.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+  #app
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+import Component from 'vue-class-component'
+
+@Component
+export default class App extends Vue {}
+</script>
